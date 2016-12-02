02acbe3b1bdbcb8857332c06e60bde0ca9f4f39f
Brian Bycynski <high5bri@hotmail.com>
2016-12-02 13:33:11 -0500

    Tic Tac 1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += ''
+  let player = 'x'
+  const cells = document.querySelectorAll('td')
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', () => {
+      cells[i].textContent = player
+      if (player === 'x') {
+        player = 'o'
+      } else {
+        player = 'x'
+      }
+    })
+  }
 }
 
 document.addEventListener('DOMContentLoaded', main)
