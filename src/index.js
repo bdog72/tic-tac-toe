63c8cb3ba38977bd467cc47b64e28da33e7aaed2
Brian Bycynski <high5bri@hotmail.com>
2016-12-04 10:31:22 -0500

    Tic Tac Toe sunday progress

--- src/index.js
+++ src/index.js
@@ -2,19 +2,47 @@ if (process.env.NODE_ENV !== 'production') require('./index.html')
 import './styles/screen.scss'
 
 const main = () => {
-  document.querySelector('h1').textContent += ''
-  let player = 'x'
+  // document.querySelector('h1').textContent += ''
+  let player = 'X'
   const cells = document.querySelectorAll('td')
   for (let i = 0; i < cells.length; i++) {
     cells[i].addEventListener('click', () => {
-      cells[i].textContent = player
-      if (player === 'x') {
-        player = 'o'
-      } else {
-        player = 'x'
+      if (cells[i].textContent === '') {
+        cells[i].textContent = player
+        if (player === 'X') {
+          player = 'O'
+        } else {
+          player = 'X'
+        }
       }
     })
   }
+}
+
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 6, 3],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+const checkForWinner = () => {
+  for (let i = 0; i < winningCombos.length; i++) {
+    const winningCombo = winningCombo[i]
+
+    let moves = winningCombo.map((position) => {
+      return cells[position].textContent
+    })
+    if (moves.every((move) => {
+      return move === player
+    })) {
+      return true
+    }
+  }
+  return false
 }
 
 document.addEventListener('DOMContentLoaded', main)
